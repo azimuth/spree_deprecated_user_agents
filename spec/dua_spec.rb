7fc92fe2e1135f334946503ed528e388127aae64
Christopher Maujean <cmaujean@gmail.com>
2011-04-06 15:09:50 -0700

    stubbing out a spec, fixing version

--- spec/dua_spec.rb
+++ spec/dua_spec.rb
@@ -1,1 +1,11 @@
 require 'spec_helper'
+
+
+describe "SpreeDeprecatedUserAgents" do
+  it "is untestable without adding more dependencies and setup" do
+    message = 
+      "This will need a javascript capable test browser that can pretend to be 
+      IE 6, including processing IE specific preprocessing like: 
+      <!--[if lte IE 6]>"
+  end
+end
--- spree_deprecated_user_agents.gemspec
+++ spree_deprecated_user_agents.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'spree_deprecated_user_agents'
-  s.version     = '0.50.0'
+  s.version     = '0.1.0'
   s.summary     = ''
   s.description = 'Add (optional) gem description here'
   s.required_ruby_version = '>= 1.8.7'
